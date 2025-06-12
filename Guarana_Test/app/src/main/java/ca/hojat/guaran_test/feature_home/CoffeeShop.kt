afa77d4febb9995b0d4024dfec15c8cf0487a113
hojat <hojat72elect@gmail.com>
2025-06-12 03:46:14 -0400

    Removed useless parts

--- Guarana_Test/app/src/main/java/ca/hojat/guaran_test/feature_home/CoffeeShop.kt
+++
@@ -1,9 +0,0 @@
-package ca.hojat.guaran_test.feature_home
-
-data class CoffeeShop(
-    val name: String,
-    val isOpen: Boolean,
-    val imageResId: Int,
-    val distance: Int = 3,
-    val description: String = ""
-)
