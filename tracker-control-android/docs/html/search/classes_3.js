1b0662f997775133f772466837e2af31bde24de2
hojat72elect <hojat72elect@gmail.com>
2025-07-10 20:04:18 -0400

    I didn't know how to make money from "APK extractor" and "tracker control android", so had to remove them.

--- tracker-control-android/docs/html/search/classes_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['expandedlistview_0',['ExpandedListView',['../classeu_1_1faircode_1_1netguard_1_1_expanded_list_view.html',1,'eu::faircode::netguard']]]
-];
--- tracker-control-android/docs/html/search/classes_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['ng_5fsession_0',['ng_session',['../structng__session.html',1,'']]]
-];
